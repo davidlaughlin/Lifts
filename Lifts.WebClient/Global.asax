26adc48555b5a91388f5ef9d8af4d979f7f02184
David Laughlin <davidlaughlin@gmail.com>
2015-04-03 20:03:40 -0500

    Initial Commit of Fresh VS Solution

---
+++ Lifts.WebClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lifts.WebClient.MvcApplication" Language="C#" %>
---
+++ Lifts.WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lifts.WebClient.Startup))]
+namespace Lifts.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
